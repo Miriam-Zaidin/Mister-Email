c542c653107f867ad1aeebd9ae6e665bc12e89bd
Miriam Shaparinsky <mirizaidin@gmail.com>
2023-12-22 03:59:45 +0200

    Add email preview css

--- src/cmps/EmailPreview.jsx
+++ src/cmps/EmailPreview.jsx
@@ -1,39 +1,45 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-export function EmailPreview({ email, onRemoveEmail, onUpdateEmail}) {
+export function EmailPreview({ email, onRemoveEmail, onUpdateEmail }) {
     const [currentEmail, setCurrentEmail] = useState(email);
 
     useEffect(() => {
-      // Update the component's state when the email prop changes
-      setCurrentEmail(email);
+        // Update the component's state when the email prop changes
+        setCurrentEmail(email);
     }, [email]);
-    
-    let starClass = currentEmail.isStarred ? 'is-starred' : '';
-    let starIcon = currentEmail.isStarred ? "fa fa-star" : "fa fa-star-o";
-    let isReadIcon = currentEmail.isRead ? "fa fa-envelope-o" : "fa fa-envelope-open-o";
+
 
     async function onToggle(ev) {
         const { name: field } = ev.target
         const updatedEmail = { ...currentEmail, [field]: !currentEmail[field] }
         onUpdateEmail(updatedEmail)
     }
+    let starClass = currentEmail.isStarred ? 'is-starred' : '';
+    let starIcon = currentEmail.isStarred ? "fa fa-star" : "fa fa-star-o";
+    let isReadIcon = currentEmail.isRead ? "fa fa-envelope-o" : "fa fa-envelope-open-o";
+    let sentAt=new Date(currentEmail.sentAt).toLocaleString()
     return (
-        <article className="email-preview">
-            <Link to={`/email/${currentEmail.id}`}>
+
+        <Link to={`/email/${currentEmail.id}`}>
+            <article className="email-preview">
                 <button onClick={onToggle} name="isStarred" className={starClass}>
                     <i className={starIcon}></i>
                 </button>
-                <h2>{currentEmail.from}</h2>
-                <p>{currentEmail.subject}</p>
-                <button onClick={onToggle} name="isRead">
-                    <i className={isReadIcon}></i>
-                </button>
+                <span>{currentEmail.from}</span>
+                <span><b>{currentEmail.subject}</b> - {currentEmail.body}</span>
 
-                <button onClick={()=>onRemoveEmail(currentEmail.id)}>
-                    <i className="fa fa-trash-o"></i>
-                </button>
-            </Link>
-        </article>
+                <span className="span-sent-at">{sentAt}</span>
+                <div className="email-actions">
+                    <button onClick={onToggle} name="isRead">
+                        <i className={isReadIcon}></i>
+                    </button>
+                    <button onClick={() => onRemoveEmail(currentEmail.id)}>
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                </div>
+            </article>
+        </Link >
+
     )
 }
